5e09265523337ddb50333e488cc7c716efa6c186
James24722 <103395771+James24722@users.noreply.github.com>
2022-04-19 16:29:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//James
+
 function myFunction() {
       var x = document.getElementById("myText").value;//Gets the shuz of the text
       document.getElementById("demo").innerHTML =("welcome   "+ x+"  !");
