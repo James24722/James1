dae857808ae9e51c81a7f74505472f6e35c50d31
James24722 <103395771+James24722@users.noreply.github.com>
2022-04-19 16:14:33 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function myFunction() {
+      var x = document.getElementById("myText").value;//Gets the shuz of the text
+      document.getElementById("demo").innerHTML =("welcome   "+ x+"  !");
+        align="center";
+    }
+
+function calc(func){
+    var result = document.getElementById("result");
+     var num1 = parseInt(document.getElementById("num1").value);
+     var num2 = parseInt(document.getElementById("num2").value);//Get text numbers 1 and number 2
+     if(isNaN(num1) || isNaN(num2)){
+     alert("Please enter a number");
+     return false;//Determine if it is a number
+    }
+     result.value = func(num1,num2);
+    }
+    function add(num1,num2){
+    return num1+num2;//The numbers are added
+    }
